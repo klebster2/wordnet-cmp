3286f492fb8ec766d8f72327c796c109532f06fe
Kleber Noel <42589399+klebster2@users.noreply.github.com>
2024-11-19 12:42:54 +0000

    pylint

--- python/plugin.py
+++ python/plugin.py
@@ -150,7 +150,7 @@ class WordNetCompleter:
         """Get all synonyms for a specific word sense with their definitions."""
         return [(lemma, synset.definition()) for lemma in synset.lemmas()]
 
-    def _explore_synset(  # pylint: disable=too-many-locals
+    def _explore_synset(  # pylint: disable=too-many-locals,too-many-positional-arguments
         self,
         synset: "wn.Synset",
         word_class: WordClass,
